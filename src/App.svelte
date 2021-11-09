549346e10736ca31d71338ade3099916dcb8110a
Joe Cowton <joe.cowton@guardian.co.uk>
2021-11-09 16:37:53 +0000

    add params as separate params: :

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
 import type { ArticleFormat } from "@guardian/libs";
-import { defaultValues, format } from "./stores";
+import { format } from "./stores";
 import Dropdowns from "./Dropdowns.svelte";
 
-let oldValues: ArticleFormat = defaultValues;
+format.subscribe((values: ArticleFormat) => {
+  chrome.tabs.getSelected(null, (tab) => {
+    let url = new URL(tab.url);
 
-format.subscribe((values) => {
-  if (values !== oldValues) {
-    chrome.tabs.getSelected(null, (tab) => {
-      let url = tab.url.split("?");
-      let base = url[0];
-      let newUrl =
-        base + `?format=${values.design}${values.display}${values.theme}`;
-      chrome.tabs.update(tab.id, { url: newUrl });
-    });
-    oldValues = values;
-  }
+    url.searchParams.set("design", `${values.design}`);
+    url.searchParams.set("display", `${values.display}`);
+    url.searchParams.set("theme", `${values.theme}`);
+
+    chrome.tabs.update(tab.id, { url: url.href });
+  });
 });
 </script>
 
--- src/stores.ts
+++ src/stores.ts
@@ -1,6 +1,3 @@
-import type { ArticleFormat } from '@guardian/libs';
 import { writable } from 'svelte/store';
 
-export const defaultValues: ArticleFormat = {design: 0, display: 0, theme: 0}
-
-export const format = writable(defaultValues);
+export const format = writable({design: 0, display: 0, theme: 0});
